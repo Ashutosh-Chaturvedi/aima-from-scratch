c3306242633ded1f6aaac106a7df71e21c4d64fc
Ashutosh-Chaturvedi <ashutoshchaturvedi734@gmail.com>
2026-01-16 23:53:05 +0530

    Add UCS runner for GridWorld

---
+++ search/run_ucs.py
@@ -0,0 +1,23 @@
+from search.uninformed.uniform_cost_search import uniform_cost_search
+from search.problems.gridworld import GridWorld
+
+def main():
+    problem = GridWorld(
+        initial_state=(0,0),
+        goal_state=(2,2),
+        width=3,
+        height=3
+    )
+
+    solution = uniform_cost_search(problem)
+
+    if solution:
+        print("Solution Found!")
+        print("Actions:", solution.solution())
+        print("Path:", [n.state for n in solution.path()])
+        print("Cost: ", solution.path_cost)
+    else:
+        print("No solution found.")
+        
+if __name__ == "__main__":
+    main()
